1cf2ca2f1d6be1c59acbc912e49591f5b03c3d5c
RobertoCiurea <robertociureaionut@gmail.com>
2026-06-16 20:14:53 +0300

    Updated import/export components file

--- client/app/components/components.ts
+++ client/app/components/components.ts
@@ -2,5 +2,7 @@ import {Header} from "@/app/components/Header";
 import {Hero} from "@/app/components/Hero";
 import {Navigation} from "@/app/components/Navigation";
 import {About} from "@/app/components/About";
+import {Services} from "@/app/components/Services";
+import {Categories} from "@/app/components/Categories";
 
-export {Header, Hero, Navigation, About};
+export {Header, Hero, Navigation, About, Services, Categories};
