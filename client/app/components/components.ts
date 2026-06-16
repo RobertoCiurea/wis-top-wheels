7cb18fc4dd29e337d7b3479b90e62cbd02057e3b
RobertoCiurea <robertociureaionut@gmail.com>
2026-06-16 18:18:19 +0300

    Added about component for import/export file

--- client/app/components/components.ts
+++ client/app/components/components.ts
@@ -1,5 +1,6 @@
 import {Header} from "@/app/components/Header";
 import {Hero} from "@/app/components/Hero";
 import {Navigation} from "@/app/components/Navigation";
+import {About} from "@/app/components/About";
 
-export {Header, Hero, Navigation}
+export {Header, Hero, Navigation, About};
