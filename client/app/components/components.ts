e6d62f7ee227aeb32ec9d163a671ad5c9824ee75
RobertoCiurea <robertociureaionut@gmail.com>
2026-06-16 12:05:07 +0300

    Added file that manages imports/exports of react components

---
+++ client/app/components/components.ts
@@ -0,0 +1,5 @@
+import {Header} from "@/app/components/Header";
+import {Hero} from "@/app/components/Hero";
+import {Navigation} from "@/app/components/Navigation";
+
+export {Header, Hero, Navigation}
