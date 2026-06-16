58c51a5558b5eb281429692cf2242f1b95916351
RobertoCiurea <robertociureaionut@gmail.com>
2026-06-16 14:05:33 +0300

    Added new component to the page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
 import {Header, Hero} from "@/app/components/components";
+import { Services } from "./components/Services";
 export default function Home() {
   return (
     <> 
       <Header/>
       <Hero/>
+      <Services/>
     </>
   );
 }
