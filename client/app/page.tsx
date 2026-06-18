f65bf452b3007bab27b888a037a950d24f25f085
RobertoCiurea <robertociureaionut@gmail.com>
2026-06-18 13:01:30 +0300

    added sections with relevant ids for compoenents to improve SEO

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,18 +1,33 @@
 import {Header, Hero, About, Services, Categories, WheelsStock, CarsStock, WhyUs, Cta, Contact, Map, Footer, WhatsappFloat} from "@/app/components/components";
+
 export default function Home() {
   return (
-    <> 
+    <>
       <Header/>
-      <Hero/>
-      <Services/>
-      <About/>
-      <Categories/>
-      <WheelsStock/>
-      <CarsStock/>
-      <WhyUs/>
-      <Cta/>
-      <Contact/>
-      <Map/>
+      <main>
+        <Hero/>
+        <Services/>
+        <section id="despre-noi" className="section">
+          <About/>
+        </section>
+        <Categories/>
+        <section id="stoc-jante-anvelope">
+          <WheelsStock/>
+        </section>
+        <section id="masini-vanzare">
+          <CarsStock/>
+        </section>
+        <section id="de-ce-noi">
+          <WhyUs/>
+        </section>
+        <Cta/>
+        <section id="contact-section">
+          <Contact/>
+        </section>
+        <section id="locatie">
+          <Map/>
+        </section>
+      </main>
       <Footer/>
       <WhatsappFloat/>
     </>
