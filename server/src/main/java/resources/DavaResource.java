13960cf65616929b53eeefbacd98ce0bd6172703
RobertoCiurea <robertociureaionut@gmail.com>
2026-07-28 19:12:37 +0300

    Protected route

--- server/src/main/java/resources/DavaResource.java
+++ server/src/main/java/resources/DavaResource.java
@@ -1,4 +1,23 @@
 package resources;
 
+import io.quarkus.security.Authenticated;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+@Path("/api/dashboard")
+@Authenticated
+@Produces(MediaType.APPLICATION_JSON)
 public class DavaResource {
+    @Inject
+    JsonWebToken jwt;
+    @GET
+    public Response hello(){
+        String username = jwt.getName();
+        return Response.ok("Welcome, " + username).build();
+    }
 }
