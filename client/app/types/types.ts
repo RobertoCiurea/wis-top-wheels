ce746e1a19c60e8280572a1700db384c4306d187
roxir2801 <roxir2801@gmail.com>
2026-07-31 18:38:12 +0300

    prettier change

--- client/app/types/types.ts
+++ client/app/types/types.ts
@@ -1,18 +1,16 @@
 import React, { ComponentType, ElementType } from "react";
-
 export type IconProps = React.SVGProps<SVGSVGElement> & {
   size?: number | string;
   title?: string;
 };
 
 export type CardProps = {
-    order: string
-    Icon: {
-        src: ComponentType<IconProps>
-        color: string,
-        size: number | string
-    } 
-    title: string
-    text: string
-
-}
+  order: string;
+  Icon: {
+    src: ComponentType<IconProps>;
+    color: string;
+    size: number | string;
+  };
+  title: string;
+  text: string;
+};
