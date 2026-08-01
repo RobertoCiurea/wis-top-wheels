21369bae098b1f271fca7ec33d1d8fda6df77c5b
roxir2801 <roxir2801@gmail.com>
2026-08-01 18:03:00 +0300

    fixed nextjs warn

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -235,7 +235,11 @@ export default function RootLayout({
   };
 
   return (
-    <html lang="ro" className={`${barlowCondensed.variable} ${inter.variable}`}>
+    <html
+      lang="ro"
+      className={`${barlowCondensed.variable} ${inter.variable}`}
+      data-scroll-behavior="smooth"
+    >
       <head>
         <script
           type="application/ld+json"
