7c4724b6f73fda1167328092327329ed4f592e8f
roxir2801 <roxir2801@gmail.com>
2026-08-01 17:03:26 +0300

    created custom pages for dashboard content

---
+++ client/app/dashboard/new-add/page.tsx
@@ -0,0 +1,3 @@
+export default function NewAddDashboardPage() {
+  return <div>Adauga anunt nou</div>;
+}
---
+++ client/app/dashboard/rims/page.tsx
@@ -0,0 +1,3 @@
+export default function RimsDashboardPage() {
+  return <div>Anunturi jante</div>;
+}
