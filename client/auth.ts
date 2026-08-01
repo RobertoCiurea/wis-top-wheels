0b8e15c2b9a453c4688f9a24e23a4dedfd4a06c1
roxir2801 <roxir2801@gmail.com>
2026-08-01 16:47:32 +0300

    Added refresh token and expiration logic

--- client/auth.ts
+++ client/auth.ts
@@ -12,9 +12,49 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       if (account) {
-        token.accessToken = account.access_token;
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          expiresAt: account.expires_at
+            ? account.expires_at * 1000
+            : Date.now() + 300 * 1000, //create a 5 minutes expiration date
+        };
       }
-      return token;
+      //if the token has not expired return it
+      if (Date.now() < (token.expiresAt as number)) return token;
+
+      //if the token expired generate a new one
+      try {
+        const response = await fetch(
+          `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`,
+          {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams({
+              client_id: process.env.KEYCLOAK_ID!,
+              client_secret: process.env.KEYCLOAK_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: token.refreshToken as string,
+            }),
+            method: "POST",
+          },
+        );
+
+        const refreshedTokens = await response.json();
+
+        if (!response.ok) throw refreshedTokens;
+
+        return {
+          ...token,
+          accessToken: refreshedTokens.access_token,
+          expiresAt: Date.now() + refreshedTokens.expires_in * 1000,
+          // use the to old refresh token if Keycloak didn't issue a new one
+          refreshToken: refreshedTokens.refresh_token ?? token.refreshToken,
+        };
+      } catch (error) {
+        console.error("Error refreshing Keycloak Access Token:", error);
+        return { ...token, error: "RefreshTokenError" };
+      }
     },
     async session({ session, token }) {
       session.accessToken = token.accessToken;
