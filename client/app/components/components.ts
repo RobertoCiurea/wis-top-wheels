b9333005443f940cefbcfa40990b6339bc7964cb
roxir2801 <roxir2801@gmail.com>
2026-08-01 17:01:01 +0300

    added sidebar components

--- client/app/components/components.ts
+++ client/app/components/components.ts
@@ -1,16 +1,33 @@
-import {Header} from "@/app/components/Header";
-import {Hero} from "@/app/components/Hero";
-import {Navigation} from "@/app/components/Navigation";
-import {About} from "@/app/components/About";
-import {Services} from "@/app/components/Services";
-import {Categories} from "@/app/components/Categories";
-import {WheelsStock} from "@/app/components/WheelsStock";
-import {CarsStock} from "@/app/components/CarsStock";
-import {WhyUs} from "@/app/components/WhyUs";
-import {Cta} from "@/app/components/Cta";
-import {Contact} from "@/app/components/Contact";
+import { Header } from "@/app/components/Header";
+import { Hero } from "@/app/components/Hero";
+import { Navigation } from "@/app/components/Navigation";
+import { About } from "@/app/components/About";
+import { Services } from "@/app/components/Services";
+import { Categories } from "@/app/components/Categories";
+import { WheelsStock } from "@/app/components/WheelsStock";
+import { CarsStock } from "@/app/components/CarsStock";
+import { WhyUs } from "@/app/components/WhyUs";
+import { Cta } from "@/app/components/Cta";
+import { Contact } from "@/app/components/Contact";
 import { Map } from "./Map";
 import { Footer } from "./Footer";
-import { WhatsappFloat } from "./WhatsappFloat"; 
+import { WhatsappFloat } from "./WhatsappFloat";
+import { Sidebar } from "./Sidebar";
 
-export {Header, Hero, Navigation, About, Services, Categories, WheelsStock, CarsStock, WhyUs, Cta, Contact, Map, Footer, WhatsappFloat};
+export {
+  Header,
+  Hero,
+  Navigation,
+  About,
+  Services,
+  Categories,
+  WheelsStock,
+  CarsStock,
+  WhyUs,
+  Cta,
+  Contact,
+  Map,
+  Footer,
+  WhatsappFloat,
+  Sidebar,
+};
