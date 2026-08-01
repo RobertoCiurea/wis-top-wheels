9f5ba904e6efd7847ffa1e9eef91abd0338211a8
roxir2801 <roxir2801@gmail.com>
2026-08-01 16:48:39 +0300

    redirect also if there is a refresh token error

--- client/proxy.ts
+++ client/proxy.ts
@@ -3,10 +3,11 @@ import { NextResponse } from "next/server";
 
 export default auth((req) => {
   const isLoggedIn = !!req.auth;
+  const hasRefreshTokenError = req.auth?.error === "RefreshTokenError";
   const isDashboard = req.nextUrl.pathname.startsWith("/dashboard");
 
-  //redirect only if the user is on /dashboard route and not logged in
-  if (isDashboard && !isLoggedIn) {
+  //redirect only if the user is on /dashboard route and not logged in or has refresh token error
+  if (isDashboard && !(isLoggedIn || hasRefreshTokenError)) {
     const newUrl = new URL("/api/auth/signin", req.nextUrl.origin);
 
     // Callback url after keycloak login
