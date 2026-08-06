5c775ee038990f9c1d5fa289967ae1f9cfb8fda6
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-08-06 15:38:47 +0300

    created my account page

--- client/app/dashboard/account/page.tsx
+++ client/app/dashboard/account/page.tsx
@@ -1,7 +1,9 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { AccessDenied } from "@/app/components/AccessDenied";
-
+import "@/app/styles/dashboard.css";
+import { MyAccount } from "@/app/components/MyAccount";
+import { UserCardProps } from "@/app/types/types";
 export default async function AccountDashboardPage() {
   const session = await auth();
   if (!session) {
@@ -17,6 +19,19 @@ export default async function AccountDashboardPage() {
   if (response.status === 401 || response.status === 403) {
     return <AccessDenied />;
   }
-  const data = await response.json;
-  console.log(data);
+  const data = (await response.json()) as UserCardProps;
+  return (
+    <div className="dashboard-content">
+      <h1>Contul meu</h1>
+      <MyAccount
+        id={data.id}
+        username={data.username}
+        firstName={data.firstName}
+        lastName={data.lastName}
+        email={data.email}
+        roles={data.roles}
+        accessToken={session.accessToken}
+      />
+    </div>
+  );
 }
