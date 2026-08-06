26f9435ef882b6d19dbd60a020682bb9b6fd5dc4
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-08-06 20:16:05 +0300

    created form action for updating account's info

---
+++ client/app/actions/updateAccountAction.ts
@@ -0,0 +1,76 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { ActionState } from "../types/types";
+
+export async function updateAccount(
+  prevState: ActionState,
+  formData: FormData,
+): Promise<ActionState> {
+  const username = formData.get("username") as string;
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
+  const email = formData.get("email") as string;
+  const accessToken = formData.get("accessToken") as string;
+
+  if (!username || !username.trim()) {
+    return { status: 400, error: "Numele de utilizator nu poate fi gol." };
+  }
+  if (!firstName || !firstName.trim()) {
+    return { status: 400, error: "Prenumele nu poate fi gol." };
+  }
+  if (!lastName || !lastName.trim()) {
+    return { status: 400, error: "Numele nu poate fi gol." };
+  }
+  if (!email || !email.trim()) {
+    return { status: 400, error: "Emailul nu poate fi gol." };
+  }
+  const data = {
+    username: username,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+  };
+
+  try {
+    const response = await fetch(`${process.env.SERVER_URL}/api/account`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      switch (response.status) {
+        case 409:
+          return {
+            status: 409,
+            error: "Există deja un utilizator cu acest nume sau email",
+          };
+        case 401:
+        case 403:
+          return {
+            status: response.status,
+            error:
+              "Sesiunea a expirat sau nu ai drepturile necesare. Conectează-te din nou pentru a continua.",
+          };
+        default:
+          return {
+            status: response.status,
+            error: "A apărut o eroare la acutalizarea contului.",
+          };
+      }
+    }
+    revalidatePath("/dashboard/account");
+    return {
+      status: 200,
+      message: "Datele au fost actualizate cu succes!",
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      status: 500,
+      error: "Eroare de rețea. Nu s-au putut actualiza datele.",
+    };
+  }
+}
