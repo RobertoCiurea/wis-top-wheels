0f60a937ef436d35c2a2525dff4a2718279fef7a
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-08-12 18:56:28 +0300

    modified messages fields and extended dto

--- server/src/main/java/dto/BaseAdDto.java
+++ server/src/main/java/dto/BaseAdDto.java
@@ -2,14 +2,23 @@ package dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
 
 public abstract class BaseAdDto {
 
-    @NotBlank(message = "Title cannot be empty")
+    @NotBlank(message = "Titlul anunțului este obligatoriu.")
+    @Size(min = 16, max = 70, message = "Titlul trebuie să aibă între 16 și 70 de caractere.")
         public String title;
-    @NotBlank(message = "Description cannot be empty")
+    @NotBlank(message = "Descrierea este obligatorie.")
+    @Size(min = 40, max = 9000, message ="Descrierea trebuie să aibă minim 40 de caractere." )
         public String description;
 
-    @Positive(message = "Price cannot be negative")
+    @Positive(message = "Prețul nu poate fi negativ")
     public double price;
+
+    public String state; //new / used
+
+    public List<String> imageUrls;
 }
