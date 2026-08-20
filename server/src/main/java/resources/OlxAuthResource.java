3eebb986d8d595f69ba200eb852acd743eda6ad1
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-08-20 16:41:30 +0300

    fixed route response and added status route

--- server/src/main/java/resources/OlxAuthResource.java
+++ server/src/main/java/resources/OlxAuthResource.java
@@ -9,13 +9,16 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import java.net.URI;
 import java.time.Instant;
+import java.util.Map;
 
 @Path("/api/olx/auth")
 public class OlxAuthResource {
@@ -28,6 +31,9 @@ public class OlxAuthResource {
     @ConfigProperty(name = "OLX_REDIRECT_URL_BASE")
     String redirectUriBase;
 
+    @ConfigProperty(name = "NEXT_BASE_URL")
+    String nextUriBase;
+
     @Inject
             @RestClient
     OlxAuthClient olxAuthClient;
@@ -35,6 +41,9 @@ public class OlxAuthResource {
     @GET
     @Path("/setup")
     @PermitAll
+    //change it later in production
+    //@RolesAllowed({"admin", "moderator"})
+
     @Transactional
     public Response setupOlx(@QueryParam("code") String code){
         String redirectUri = redirectUriBase + "/api/olx/auth/setup";
@@ -42,9 +51,9 @@ public class OlxAuthResource {
             if(code == null){
                 String url = "https://www.olx.ro/oauth/authorize/?client_id=" + clientId +
                         "&response_type=code&scope=v2 read write&redirect_uri=" + redirectUri;
-                String html = "<h2>Setup OLX WIS Top Wheels</h2><a href='" + url + "'>Click Aici pentru a Autoriza Aplicatia</a>";
-                return Response.ok(html).type(MediaType.TEXT_HTML).build();
+                return Response.ok(url).type(MediaType.TEXT_PLAIN).build();
             }else{
+
                 OlxTokenResponse response = olxAuthClient.fetchToken(
                         "authorization_code",
                         clientId,
@@ -62,12 +71,32 @@ public class OlxAuthResource {
                 tokenRecord.refreshToken = response.refreshToken;
                 tokenRecord.updatedAt = Instant.now();
                 tokenRecord.persist();;
-                return Response.ok().entity(tokenRecord).build();
+                URI frontendRedirect = URI.create(nextUriBase + "/dashboard?olx=success");
+                return Response.seeOther(frontendRedirect).build();
             }
         }catch (Exception e){
             e.printStackTrace();
-            return Response.serverError().entity("Something went wrong with the OLX API connection").build();
+            URI frontendError = URI.create(nextUriBase + "/dashboard?olx=error");
+            return Response.seeOther(frontendError).build();
         }
 
+    }
+
+
+    @Path("/status")
+    @GET
+    @PermitAll //change later to RolesAllowed
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getOlxConnectionStatus(){
+        try{
+            OlxTokenEntity tokenRecord = OlxTokenEntity.findById("SINGLETON");
+                boolean isConnected = (tokenRecord!=null
+                        && tokenRecord.refreshToken!=null
+                        && !tokenRecord.refreshToken.trim().isEmpty());
+                return Response.ok(Map.of("isConnected", isConnected)).build();
+            }catch (Exception e){
+            e.printStackTrace();
+            return Response.serverError().entity("Error connecting "+ e.getMessage()).build();
+        }
     }
 }
