c2de41ecad04fd19f2db97eeabb31d67d467b71e
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-08-25 14:15:24 +0300

    created catalog page for wheel adverts

--- client/app/anunturi/jante-si-roti/page.tsx
+++ client/app/anunturi/jante-si-roti/page.tsx
@@ -1,3 +1,38 @@
-export default async function WheelAdverts() {
-  return <h1>Anunturi</h1>;
+import { WheelsStock } from "@/app/components/WheelsStock";
+import { WheelAdvertFilters } from "@/app/components/WheelAdvertFilters";
+import { getWheelAdverts } from "@/services/advertService";
+import { Suspense } from "react";
+
+export default async function WheelAdverts({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const filters = await searchParams;
+  const params = {
+    page: filters.page ? Number(filters.page) : 1,
+    limit: 12,
+    category: filters.category,
+    maxPrice: filters.maxPrice,
+    state: filters.state,
+    diameter: filters.diameter,
+    make: filters.make,
+    material: filters.material,
+    tyreBrand: filters.tyreBrand,
+    season: filters.season,
+    width: filters.width,
+    profile: filters.profile,
+    sortBy: filters.sortBy,
+    order: filters.order,
+  };
+  const data = await getWheelAdverts(params);
+  return (
+    <Suspense fallback={"Se incarca"}>
+      <WheelsStock
+        wheelAdverts={data?.items}
+        total={data?.total}
+        limit={params.limit}
+      />
+    </Suspense>
+  );
 }
