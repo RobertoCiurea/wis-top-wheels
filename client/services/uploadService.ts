fdbce20479557ce5568286602f75cecb92120787
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-09-02 13:20:51 +0300

    updated error handling

--- client/services/uploadService.ts
+++ client/services/uploadService.ts
@@ -1,24 +1,30 @@
 import { generatePresignedUrl } from "@/app/actions/presignedUrlAction";
 
 export async function uploadFilesToSTorage(files: File[]): Promise<string[]> {
-  const uploadedUrls: string[] = [];
-  for (const file of files) {
-    const { presignedUrl, objectUrl } = await generatePresignedUrl(
-      file.name,
-      file.type,
-    );
-    const uploadResponse = await fetch(presignedUrl, {
-      method: "PUT",
-      body: file,
-      headers: {
-        "Content-Type": file.type,
-      },
-    });
+  try {
+    const uploadedUrls: string[] = [];
+    for (const file of files) {
+      const { presignedUrl, objectUrl } = await generatePresignedUrl(
+        file.name,
+        file.type,
+      );
+      const uploadResponse = await fetch(presignedUrl, {
+        method: "PUT",
+        body: file,
+        headers: {
+          "Content-Type": file.type,
+        },
+      });
 
-    if (!uploadResponse.ok) {
-      throw new Error("Error uploading file " + file.name);
+      if (!uploadResponse.ok) {
+        console.log(uploadResponse);
+        throw new Error("Error uploading file " + file.name);
+      }
+      uploadedUrls.push(objectUrl);
     }
-    uploadedUrls.push(objectUrl);
+    return uploadedUrls;
+  } catch (error) {
+    console.error("Error uploading files:", error);
+    throw error;
   }
-  return uploadedUrls;
 }
