7932c27ca8ebe17e58247d3ad9e6f093d1bf8edb
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-09-02 22:01:54 +0300

    created client that handles google recaptcha rest api

--- server/src/main/java/client/CaptchaClient.java
+++ server/src/main/java/client/CaptchaClient.java
@@ -1,4 +1,23 @@
 package client;
 
+import dto.RecaptchaRequestDto;
+import dto.RecaptchaResponseDto;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(configKey = "recaptcha-enterprise")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/v1/projects")
 public interface CaptchaClient {
+
+    @POST
+    @Path("/{projectId}/assessments")
+    RecaptchaResponseDto createAssessment(
+            @PathParam("projectId") String projectId,
+            @QueryParam("key") String apiKey,
+            RecaptchaRequestDto recaptchaPayload
+    );
+
 }
