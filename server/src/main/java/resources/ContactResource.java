a4625567e2eef841b9c5b68aa48f0961072b25ff
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-09-08 16:07:54 +0300

    fixed dynamic route issue

--- server/src/main/java/resources/ContactResource.java
+++ server/src/main/java/resources/ContactResource.java
@@ -24,7 +24,7 @@ public class ContactResource {
     }
 
     @GET
-    @Path("/id")
+    @Path("/{id}")
     public Response getContactById(@PathParam("id")Long id){
         ContactEntity contact = ContactEntity.findById(id);
         if(contact == null){
