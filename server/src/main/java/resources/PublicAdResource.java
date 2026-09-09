dac78498bfc27b98048f6814a11eed85f162c94e
RobertoCiurea <ciurearobertoionut@hotmail.com>
2026-09-09 21:20:42 +0300

    removed logs, unused injected mapper service and used inline variable

--- server/src/main/java/resources/PublicAdResource.java
+++ server/src/main/java/resources/PublicAdResource.java
@@ -12,7 +12,6 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import service.OlxMapperService;
 import service.OlxTokenManager;
 
 import java.util.stream.Collectors;
@@ -29,9 +28,6 @@ public class PublicAdResource {
     @RestClient
     OlxAdClient adClient;
 
-    @Inject
-    OlxMapperService mapper;
-
     @GET
     @Path("/wheels")
     @CacheResult(cacheName = "public-ads-list")
@@ -41,12 +37,10 @@ public class PublicAdResource {
         try{
             String authHeader = "Bearer " + tokenManager.getAccessToken();
             OlxAdListResponseDto response = adClient.getAds(authHeader, "2.0");
-            var activeAds = response.data.stream()
+
+            response.data= response.data.stream()
                     .filter(ad -> "active".equalsIgnoreCase(ad.status))
                     .toList();
-            Log.info("Anunturi active");
-            Log.info(activeAds);
-            response.data=activeAds;
             return Response.ok().entity(response).build();
         }catch (WebApplicationException e){
             String error = e.getResponse().readEntity(String.class);
